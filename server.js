711e30e0d068152803d6c1d70b72c34d7fa5bab5
ThrupthiShetty <thrupthishetty@gmail.com>
2019-06-11 15:41:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color:orange">Hello world</h2> <p style="color:blue">Welcome to Node app</p>')
+  res.send('<h2 style="color:orange">Hello world</h2> <p style="color:blue">Welcome to Node app... </p><p style="color:red">Welcome to OKE... </p>')
 })
 app.listen(8084, () => {
   console.log('App listening on port 8084');
