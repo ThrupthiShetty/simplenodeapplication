6ecab75b610343cacafaea949655a22972339008
ThrupthiShetty <thrupthishetty@gmail.com>
2019-07-08 15:36:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ Simple Demo
         </title>
     </head>
     <body style="background:lightblue">
-        <h1 style="color:indigo">Welcome To OKE Developer Assistant Tool</h1>
-        <p style="color:green">Your application is now running on OKE Cluster......</p>
+        <div>
+                <img src="https://1000logos.net/wp-content/uploads/2017/04/Oracle-Logo.png" style="width:15%">
+
+        </div>
+ <div style="    display: block;
+ text-align: center;">     
+        <h1 style="color:indigo; display: contents;">Welcome To OKE Developer Assistant Tool</h1>
+    </div>      <p style="color:green">Your application is now running on OKE Cluster......</p>
+        <img src="https://www.hopetutors.com/wp-content/uploads/2018/07/Docker-and-Kubernetes-Training-in-Chennai.jpg">
     </body>
     <footer>
         Copyright © 2018, Oracle and/or its affiliates.  All Rights Reserved. | Confidential – Oracle Internal
