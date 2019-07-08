bf40c4740364fa8b6b5a0e3e599d4cd7f5903055
ThrupthiShetty <thrupthishetty@gmail.com>
2019-07-08 15:27:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const app = require('express')();
 app.get('/', (req, res) => {
-  res.send('<h2 style="color:orange">Hello world</h2> <p style="color:blue">Welcome to Node app... </p><p style="color:red">Welcome to OKE... </p>')
+  res.send('<html >
+    <head>
+        <title>
+Simple Demo
+        </title>
+    </head>
+    <body style="background:lightblue">
+        <h1 style="color:indigo">Welcome To OKE Developer Assistant Tool</h1>
+        <p style="color:green">Your application is now running on OKE Cluster......</p>
+    </body>
+    <footer>
+        Copyright © 2018, Oracle and/or its affiliates.  All Rights Reserved. | Confidential – Oracle Internal
+
+    </footer>
+</html>')
 })
 app.listen(8084, () => {
   console.log('App listening on port 8084');
